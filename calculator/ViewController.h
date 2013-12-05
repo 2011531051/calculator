2ac5dd5308b03e80490cdc8a8e30652fc964831f
Noritsune Riko <solico-66@i.softbank.jp>
2013-12-05 15:41:08 +0900

    ＋の計算

--- calculator/ViewController.h
+++ calculator/ViewController.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    int currentNumber, previousNumber;
+}
+
+- (IBAction)inputNumber0:(id)sender;
+- (IBAction)inputNumber1:(id)sender;
+- (IBAction)inputNumber2:(id)sender;
+- (IBAction)inputNumber3:(id)sender;
+- (IBAction)inputNumber4:(id)sender;
+- (IBAction)inputNumber5:(id)sender;
+- (IBAction)inputNumber6:(id)sender;
+- (IBAction)inputNumber7:(id)sender;
+- (IBAction)inputNumber8:(id)sender;
+- (IBAction)inputNumber9:(id)sender;
+
+- (IBAction)addition:(id)sender;
+- (IBAction)subtraction:(id)sender;
+- (IBAction)multiplication:(id)sender;
+- (IBAction)division:(id)sender;
+- (IBAction)equal:(id)sender;
+- (IBAction)AC:(id)sender;
+- (IBAction)plusminus:(id)sender;
+
+@property enum {add, sub, mul, div} state;
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOutput;
 
 @end
--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -26,4 +26,129 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)inputNumber0:(id)sender {
+    currentNumber = currentNumber * 10;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber1:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 1;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber2:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 2;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber3:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 3;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber4:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 4;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber5:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 5;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber6:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 6;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber7:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 7;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber8:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 8;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)inputNumber9:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 9;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)addition:(id)sender {
+    currentNumber = previousNumber + currentNumber;
+    previousNumber = currentNumber;
+    currentNumber = 0;
+    [self setState:add];
+    _numberOutput.text = [NSString stringWithFormat:@"%d",previousNumber];
+}
+
+- (IBAction)subtraction:(id)sender {
+    previousNumber = currentNumber;
+    currentNumber = 0;
+    [self setState:sub];
+    _numberOutput.text = [NSString stringWithFormat:@"%d",previousNumber];
+}
+
+- (IBAction)multiplication:(id)sender {
+    previousNumber = currentNumber;
+    currentNumber = 0;
+    [self setState:mul];
+    _numberOutput.text = [NSString stringWithFormat:@"%d",previousNumber];
+}
+
+- (IBAction)division:(id)sender {
+    previousNumber = currentNumber;
+    currentNumber = 0;
+    [self setState:div];
+    _numberOutput.text = [NSString stringWithFormat:@"%d",previousNumber];
+}
+
+- (IBAction)equal:(id)sender {
+    switch (self.state) {
+        case add:
+            currentNumber = previousNumber + currentNumber;
+            _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+            previousNumber = currentNumber;
+            currentNumber = 0;
+            break;
+        case sub:
+            currentNumber = previousNumber - currentNumber;
+            _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+            currentNumber = 0;
+            break;
+        case mul:
+            currentNumber = previousNumber * currentNumber;
+            _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+            currentNumber = 0;
+            break;
+        case div:
+            currentNumber = previousNumber / currentNumber;
+            _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+            currentNumber = 0;
+            break;
+    }
+}
+
+- (IBAction)AC:(id)sender {
+    currentNumber = 0;
+    previousNumber = 0;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)plusminus:(id)sender {
+    currentNumber = (-1) * currentNumber;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
 @end
